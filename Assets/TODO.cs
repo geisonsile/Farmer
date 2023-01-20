7a73b24ce669dae88225474190440e0c94003a29
geisonsile <geisonsile@gmail.com>
2023-01-20 18:11:20 -0300

    Troca do player pela versão final e ajustes gerais no jogo

---
+++ Assets/TODO.cs
@@ -0,0 +1,9 @@
+
+using UnityEngine;
+
+public class TODO : MonoBehaviour
+{
+    //Camera afastar quando parado e aproximar em movimento
+    //Colocar HUD com as moedas ganhas
+    //Colocar audios
+}
